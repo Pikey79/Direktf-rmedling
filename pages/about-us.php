d6fd417f983284c38f5de1d654a6781b6c8dbe8f
pierre <pierre.massamiri@hotmail.com>
2018-04-24 09:05:57 +0200

    fixat om oss sida med bild och text

--- pages/about-us.php
+++ pages/about-us.php
@@ -1,16 +1,19 @@
 <div class="ap-main-container">
 	<!-- Section -->
-	<div class="container-fluid padding">
+	<div class="container-fluid padding ap-more-padding">
 		<div class="row">
 			<div class="col-lg-6">
 				<h2>Om oss</h2>
-				<p>Lägg in text här.......</p>
-				<p>Och här.......</p>
+				<p>Vi vet att planer inte alltid håller. Hur noga man än förberett sig så kan det alltid uppstå en situation där det krävs en snabb och hjälpande hand. När du som kund vänder dig till Direktförmedling för personal har vi som målsättning att alltid möta dina önskemål direkt.</p>
+				<p>Vi har en omfattande och mångårig branschvana inom skola, vård och omsorg, ekonomi, lokalvård, bygg samt restaurang. Direktförmedling AB startades av tidigare lärare, ekonomer, sjuksköterskor samt bemanningskonsulter. Denna breda kompetens och erfarenhet ger oss en stor fördel i att bäst och effektivast kunna möta dig som kund och dina önskemål.</p>
+				<h2>Varför bemanning?</h2>
+				<p>-Vid plötsliga vakanser så som föräldraledighet eller sjukdom</p>
+				<p>-Vid förändring av arbetsbelastning</p>
+				<p>-Vid organisationsförändringar</p>
+				<p>Utöver att det spar dig som arbetsgivare tid på att plötsligt hitta rätt medarbetare så kan bemanning via Direktförmedling göra din verksamhet flexibel och fortsätta svara på dina kunders behov. Vi finns till för att underlätta för dig!</p>
 			</div>
 			<div class="col-lg-6">
-				<h2>Om oss</h2>
-				<p>Lägg in text här.......</p>
-				<p>Och här.......</p>
+				<img src="img/df/about-us.jpg" class="img-fluid">
 			</div>
 		</div>
 	</div>

ad5acfc9f98e617ef3f6815016ca981d16eefd29
pierre <pierre.massamiri@hotmail.com>
2018-05-10 15:20:48 +0200

    datum i admin table & knappar för ta bort rad mm

--- admin/welcome.php
+++ admin/welcome.php
@@ -20,10 +20,10 @@ session_start();
     @mysql_select_db($dbase) or die("Unable to select database");*/
     include('db.php');
 
-    $query= "SELECT id, name, phone, email, message, operation, filename FROM apply_job ORDER BY ID desc";
+    $query= "SELECT id, time, name, phone, email, message, operation, filename FROM apply_job ORDER BY ID desc";
     $result = mysqli_query($conn, $query);
 
-    $c_query= "SELECT id, company_name, name, phone, email, message FROM search_staff ORDER BY ID desc";
+    $c_query= "SELECT id, time, company_name, name, phone, email, message FROM search_staff ORDER BY ID desc";
     $c_result = mysqli_query($conn, $c_query);
 
     echo "<h1>Adminpanel</h1><a href='logout.php' class='logout'>Logga ut</a><hr><h2>Jobbansökan</h2>";
@@ -31,15 +31,18 @@ session_start();
     print "<table border=0 class='CV-table'>\n";
     print "<tr>
       <th>ID</th>
+      <th>Datum</th>
       <th>Namn</th>
       <th>Telefon</th>
       <th>Email</th>
       <th>Meddelande</th>
       <th>Verksamhet</th>
       <th>CV/Personligtbrev</th>
+      <th>Redigera rad</th>
     </tr>";
     while ($row = mysqli_fetch_array($result)){
       $form_id = $row['id'];
+      $form_time = $row['time'];
       $form_name = $row['name'];
       $form_phone = $row['phone'];
       $form_email = $row['email'];
@@ -50,6 +53,9 @@ session_start();
       print "<tr>\n";
       print "\t<td>\n";
       echo "<div>$form_id</div>";
+      print "</td>\n";
+      print "\t<td>\n";
+      echo "<div>$form_time</div>";
       print "</td>\n";
       print "\t<td>\n";
       echo "<div>$form_name</div>";
@@ -102,6 +108,9 @@ session_start();
       print "\t<td>\n";
       echo "<div align=center><a class='link-CV' href='$files_show'>Personligtbrev / CV</a></div>";
       print "</td>\n";
+      print "\t<td>\n";
+      echo "<button type='button' class='btn respond-btn'>Svarat</button><button type='button' class='btn delete-btn'>Radera</button>";
+      print "</td>\n";
       print "</tr>\n";
     }
     print "</table>\n";
@@ -114,14 +123,17 @@ session_start();
     print "<table border=0 class='CV-table'>\n";
     print "<tr>
       <th>ID</th>
+      <th>Datum</th>
       <th>Företagsnamn</th>
       <th>Namn</th>
       <th>Telefon</th>
       <th>Email</th>
       <th>Meddelande</th>
+      <th>Redigera rad</th>
     </tr>";
     while ($c_row = mysqli_fetch_array($c_result)){
       $c_form_id = $c_row['id'];
+      $c_form_time = $c_row['time'];
       $c_form_company_name = $c_row['company_name'];
       $c_form_name = $c_row['name'];
       $c_form_phone = $c_row['phone'];
@@ -130,6 +142,9 @@ session_start();
       print "<tr>\n";
       print "\t<td>\n";
       echo "<div>$c_form_id</div>";
+      print "</td>\n";
+      print "\t<td>\n";
+      echo "<div>$c_form_time</div>";
       print "</td>\n";
       print "\t<td>\n";
       echo "<div>$c_form_company_name</div>";
@@ -145,6 +160,9 @@ session_start();
       print "</td>\n";
       print "\t<td>\n";
       echo "<div>$c_form_message</div>";
+      print "</td>\n";
+      print "\t<td>\n";
+      echo "<button type='button' class='btn respond-btn'>Svarat</button><button type='button' class='btn delete-btn'>Radera</button>";
       print "</td>\n";
       print "</tr>\n";
     }

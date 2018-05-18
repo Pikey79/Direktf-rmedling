45515d0082aea9862d7979aab974866a8186320e
pierre <pierre.massamiri@hotmail.com>
2018-05-18 15:28:55 +0200

    sidor för anställda med form

--- employee/retail.php
+++ employee/retail.php
@@ -1,15 +1,53 @@
+
 <div class="ap-main-container">
 
 
   <div class="ap-main-container">
-  	<div class="container-fluid padding ap-more-padding">
-  		<div class="row">
-  			<div class="col-lg-6">
-  				<h2>Detaljhandel</h2>
-  				<p>Direktförmedling hyr ut servicepersonal till restaurang- och hotellbranschen. Vi hyr även ut receptionister.
-  					Behöver du personal eller söker du jobb? Kontakta verksamhetsansvarig Reza Djam på reza@direktformedling.se eller ring <a href="tel:0707825306">(+46)70 - 782 53 06</a>.</p>
-  			</div>
-  			<div class="col-lg-6">
-  			</div>
-  		</div>
-  	</div>
+    <div class="container-fluid padding ap-more-padding">
+      <div class="row">
+        <div class="col-lg-6">
+          <h2>Detaljhandel</h2>
+          <p class="responsible-heading">Verksamhetsansvarig Hotell &amp; Restaurang, Detaljhandel och Lokalvård</p>
+          <p>Reza Djam</p>
+          <a href="tel:0707825306"><p>(+46)70 - 782 53 06</p></a>
+          <p>reza@direktformedling.se</p>
+        </div>
+        <div class="col-lg-6">
+          <div style="overflow-x:auto;">
+            <table class="table table-striped">
+              <thead class="employee-th">
+                <tr>
+                  <th>Plats</th>
+                  <th>Datum</th>
+                  <th>Tid</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                  include('../admin/db.php');
+
+                  $query= "SELECT * FROM retail_time ORDER BY ID desc";
+                  $result = mysqli_query($conn, $query);
+
+                  while ($row = mysqli_fetch_array($result)){
+                    $form_id = $row['id'];
+                    $form_place = $row['place'];
+                    $form_date = $row['date'];
+                    $form_time = $row['time'];
+                    $form_marked = $row['marked'];
+                    echo '
+                      <tr>
+                        <td>'.$row['place'].'</td>
+                        <td>'.$row['date'].'</td>
+                        <td>'.$row['time'].'</td>
+                        <td class="marked-row">'.$row['marked'].'</td>
+                      </tr>
+                    ';
+                  }
+                 ?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
